a51de283a1ab2fe3e30084b7bd16de9efbc84a33
Jeongyounghyeon <snorlax1106@naver.com>
2025-04-04 15:03:11 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/site/midmafia/midmafia/MidmafiaApplication.java
@@ -0,0 +1,13 @@
+package site.midmafia.midmafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidmafiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidmafiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/midmafia/midmafia/MidmafiaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.midmafia.midmafia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidmafiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
