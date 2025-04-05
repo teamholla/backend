aa51ee550a5049a38639d5c4060b334a9af11b48
Jeongyounghyeon <snorlax1106@naver.com>
2025-04-05 17:31:42 +0900

    test: 테스트 컨텍스트 간소화

--- src/test/java/site/midmafia/midmafia/MidmafiaApplicationTests.java
+++ src/test/java/site/midmafia/midmafia/MidmafiaApplicationTests.java
@@ -2,8 +2,9 @@ package site.midmafia.midmafia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class MidmafiaApplicationTests {
 
     @Test
